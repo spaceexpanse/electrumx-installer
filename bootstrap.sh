4c911da028d14ae047445ea3f07577d612d4317f
Walter Philips <97041967+spaceexpanse@users.noreply.github.com>
2022-04-29 11:58:26 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,10 +9,10 @@ if which git > /dev/null 2>&1; then
     cd ~/.electrumx-installer/
 else
     which wget > /dev/null 2>&1 && which unzip > /dev/null 2>&1 || { echo "Please install git or wget and unzip" && exit 1 ; }
-    wget https://github.com/bauerj/electrumx-installer/archive/master.zip -O /tmp/electrumx-master.zip
-    unzip /tmp/electrumx-master.zip -d ~/.electrumx-installer
-    rm /tmp/electrumx-master.zip
-    cd ~/.electrumx-installer/electrumx-installer-master/ 
+    wget https://github.com/spaceexpanse/electrumx-installer/archive/refs/heads/electrumx-installer-python38.zip -O /tmp/electrumx-installer-python38.zip
+    unzip /tmp/electrumx-installer-python38.zip -d ~/.electrumx-installer
+    rm /tmp/electrumx-installer-python38.zip
+    cd ~/.electrumx-installer/electrumx-installer-python38/ 
 fi
 if [[ $EUID -ne 0 ]]; then
     which sudo > /dev/null 2>&1 || { echo "You need to run this script as root" && exit 1 ; }
